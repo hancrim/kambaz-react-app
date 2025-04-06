87ad7592a0af63b008a841370b39cada956bd6c8
hancrim <han_crim@yahoo.com>
2025-04-06 19:26:07 -0400

    sign up drop down & quiz editor intial commit

---
+++ src/Kambaz/Courses/Quizzes/QuizDetailsEditor.tsx
@@ -0,0 +1,10 @@
+export default function QuizDetailsEditor() {
+  return (
+    <div>
+      <h1>Details</h1>
+      <div className="wd-quiz-details-editor">
+        {/* Quiz details editor goes here */}
+      </div>
+    </div>
+  );
+}
---
+++ src/Kambaz/Courses/Quizzes/QuizQuestionEditor.tsx
@@ -0,0 +1,10 @@
+export default function QuizQuestionEditor() {
+  return (
+    <div>
+      <h1>Questions</h1>
+      <div className="wd-quiz-question-editor">
+        {/* Quiz question editor goes here */}
+      </div>
+    </div>
+  );
+}
