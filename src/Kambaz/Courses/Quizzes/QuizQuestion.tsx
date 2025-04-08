d6fde0d968eec0a7e72b640d8b23eaa427de44fc
duncanmayer <duncmayer@gmail.com>
2025-04-08 16:50:51 -0400

    add bookmark icon (not interactable yet)

--- src/Kambaz/Courses/Quizzes/QuizQuestion.tsx
+++ src/Kambaz/Courses/Quizzes/QuizQuestion.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
-
+import { FaBookmark } from "react-icons/fa";
 const QuestionBox = ({
   currentQuestion,
   question,
@@ -10,38 +10,43 @@ const QuestionBox = ({
   question: any;
 }) => {
   return (
-    <div>
-    <Container
-      fluid
-      className="border border-black d-flex flex-column"
-    >
-      <Row
-        className="bg-light border-bottom border-black align-items-center justify-content-right"
-        style={{ height: "15%" }}
-      >
-        <Col className="p-2 justify content-center alighn-items-center">
-          <h4>Question {currentQuestion}</h4>
-        </Col>
-      </Row>
+    <Row className="align-items-start">
+      <Col xs="auto" className="pt-2 pe-0">
+        <FaBookmark></FaBookmark>
+      </Col>
 
-      <Row className="bg-white" style={{ height: "20%", padding: "1rem" }}>
-        <Col>{question.body || "question body"}</Col>
-      </Row>
+      <Col>
+        <Container fluid className="border border-black d-flex flex-column">
+          <Row
+            className="bg-light border-bottom border-black align-items-center"
+            style={{ height: "15%" }}
+          >
+            <Col className="p-2 text-center">
+              <h4>Question {currentQuestion}</h4>
+            </Col>
+          </Row>
 
-      <Row
-        className="bg-white"
-        style={{ height: "15%", padding: "1rem", overflowY: "auto" }}
-      >
-        <Col>
-          <ListGroup>
-            {question.answers.map((answer: any, index: any) => (
-              <ListGroup.Item key={index}>{answer.answer_text}</ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Col>
-      </Row>
-    </Container>
-    </div>
+          <Row className="bg-white" style={{ height: "20%", padding: "1rem" }}>
+            <Col>{question.body || "question body"}</Col>
+          </Row>
+
+          <Row
+            className="bg-white"
+            style={{ height: "15%", padding: "1rem", overflowY: "auto" }}
+          >
+            <Col>
+              <ListGroup>
+                {question.answers.map((answer: any, index: any) => (
+                  <ListGroup.Item key={index}>
+                    {answer.answer_text}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
+    </Row>
   );
 };
 
